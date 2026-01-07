ea917b64826099922a90e023238438c197a86a93
KekmaTime <22am014@sctce.ac.in>
2026-01-08 01:05:25 +0530

    feat(blockchain): queue anchoring and verify on-chain

--- apps/api/src/modules/certificates/certificates.module.ts
+++ apps/api/src/modules/certificates/certificates.module.ts
@@ -7,9 +7,11 @@ import { PdfGeneratorService } from './pdf-generator.service';
 import { QrCodeService } from './qrcode.service';
 import { VerificationService } from './verification.service';
 import { RevocationService } from './revocation.service';
+import { QueueModule } from '../queue/queue.module';
+import { BlockchainModule } from '../blockchain/blockchain.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, QueueModule, BlockchainModule],
   controllers: [CertificatesController],
   providers: [
     CertificatesService,
